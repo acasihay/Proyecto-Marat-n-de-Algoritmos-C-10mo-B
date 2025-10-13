58e4a3caccbb830e7f5d960d437360ced6511f35
jeru <147207895+acasihay@users.noreply.github.com>
2025-10-13 13:17:05 -0500

    Create concatenar dos cadenas.cpp

---
+++ concatenar dos cadenas.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b; if(!(cin>>ws); !getline(cin,a)) return 0; getline(cin,b);
+    // si entrada complicada usamos >> para dos tokens simples:
+    // cin>>a>>b;
+    cout<<a<<b<<'\n';
+    return 0;
+}
