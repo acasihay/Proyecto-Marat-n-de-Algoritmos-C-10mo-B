d6bf595b4d7628ac6cb99688de0d306b726cb5d9
jeru <147207895+acasihay@users.noreply.github.com>
2025-10-13 13:01:54 -0500

    Create multiplicacion de dos numeros.cpp

---
+++ multiplicacion de dos numeros.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long double a,b; if(!(cin>>a>>b)) return 0;
+    cout << fixed << setprecision(6) << (a*b) << '\n';
+    return 0;
+}
