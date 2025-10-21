ea3b600946ae815a2629dae365346d7d1b015dd2
jeru <147207895+acasihay@users.noreply.github.com>
2025-10-20 20:27:20 -0500

    Create conversion de decimal a binario.cpp

---
+++ conversion de decimal a binario.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    string b;
+    while(n){ b.push_back((n%2)+'0'); n/=2; }
+    reverse(b.begin(),b.end());
+    cout<<b<<'\n';
+}
